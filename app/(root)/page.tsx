4ad23d7502884e2c2b6cf09f47959ea4ce7e296e
mohamed younes <bettiouim77@gmail.com>
2023-10-09 17:23:52 +0100

    first commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,7 @@ export default async function Home({searchParams:{limit}}:{searchParams:{limit:n
  
 
   return (<><main className="relative w-full  bg-dark-4 rounded-2xl  p-10
-  max-md:p-4
+  max-md:p-4 
    max-w-3xl flex flex-col gap-4 mx-auto">
 
       {allthrads?.map((e,i):any=>(
